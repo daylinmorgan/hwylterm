fad08b8cdef6394c343cc7e65ffb9df0f307cf7e
Daylin Morgan <me@dayl.in>
2025-08-20 15:41:14 -0400

    progress~bar~

--- src/hwylterm/progress.nim
+++ src/hwylterm/progress.nim
@@ -1,65 +1,111 @@
-import std/[math, terminal, strutils,]
+import std/[math, terminal, strutils, strformat, sequtils]
 import ./[bbansi, spin]
 
 type
-  ProgressBarStyle* = object ## Style for progress bar
+  ProgressStyle* = object ## Style for progress bar
     initial, final: string  ## complete and incomplete should be equal len
     complete: string
     incomplete: string
 
-  ProgressBar* = object
-    style: ProgressBarStyle
+  ProgressSegment* = enum
+    Bar, Fraction, Percent # Elapsed?
+
+  Progress* = object
+    style: ProgressStyle
     current: Natural
     total: Natural
     file: File
+    segments: seq[ProgressSegment]
 
-const defaultStyle = ProgressBarStyle(
+const defaultStyle = ProgressStyle(
     initial: "[",
     final: "]",
     complete: "-",
     incomplete: " ",
 )
 
-proc newProgressBar*(
-  total: int = 0,
-  style: ProgressBarStyle = defaultStyle
-): ProgressBar =
-  result.style = style
-  result.total = total
-  result.file = hwylConsole.file
+proc renderFraction(p: Progress): string =
+  let pad = ($p.total).len
+  result.add " "
+  result.add align($p.current, pad)
+  result.add "/"
+  result.add $p.total
+  result.add " "
 
-proc toString*(p: ProgressBar, w: Natural): string =
-  ## progress bar will extend to size of screen
-  assert w > 2
-  let innerWidth = w - 2
+proc renderPercent(p: Progress): string =
+  let perc = p.current / p.total * 100.0
+  fmt" {perc:.2f} "
+
+proc renderBar(p: Progress, width: Natural): string =
+  assert width > 2
+  let innerWidth = width - 2
   let nPieces = int(p.current / p.total * innerWidth.float)
   result.add p.style.initial
   result.add p.style.complete.repeat(nPieces)
   result.add p.style.incomplete.repeat(innerWidth - nPieces)
   result.add p.style.final
 
-proc inc*(p: var ProgressBar, v: Natural = 1) {.inline.} =
+proc renderSegment(p: Progress, width: Natural, segment: ProgressSegment): string =
+  case segment
+  of Bar: renderBar(p, width)
+  of Fraction: renderFraction(p)
+  of Percent: renderPercent(p)
+
+proc render(p: Progress, width: Natural): string =
+  if p.segments.len == 1:
+    return renderSegment(p, width, p.segments[0])
+
+  var rendered = newSeq[string](p.segments.len)
+  var barIdx = 0
+  for i, segment in p.segments:
+    if segment == Bar:
+      barIdx = i
+      continue
+    rendered[i] = renderSegment(p, width, segment)
+
+  if Bar in p.segments:
+    let remaining = mapIt(rendered, it.len).sum()
+    rendered[barIdx] = renderBar(p, width - remaining)
+
+  for r in rendered:
+    result.add r
+
+proc newProgress*(
+  total: int = 0,
+  style: ProgressStyle = defaultStyle,
+  segments: openArray[ProgressSegment] = @[Bar]
+): Progress =
+  result.style = style
+  result.total = total
+  result.file = hwylConsole.file
+  result.segments = @segments
+
+proc inc*(p: var Progress, v: Natural = 1) {.inline.} =
   inc p.current
 
-iterator progress*[T](pb: var ProgressBar, spinner: var Spinny, items: openArray[T]): T =
-  if pb.total == 0:
-    pb.total = items.len
+iterator progress*[T](p: var Progress, spinner: var Spinny, items: openArray[T]): T =
+  if p.total == 0:
+    p.total = items.len
 
   useSpinner(spinner):
 
     for i in items:
-      spinner.setText(bbEscape(pb.toString(terminalWidth() - spinner.symbolPad)))
-      inc pb
+      spinner.setText(bbEscape(p.render(terminalWidth() - spinner.symbolPad)))
+      inc p
       yield i
 
-    if pb.current < pb.total:
-      inc pb
-      spinner.setText(bbEscape(pb.toString(terminalWidth() - spinner.symbolPad)))
+    if p.current < p.total:
+      inc p
+      spinner.setText(bbEscape(p.render(terminalWidth() - spinner.symbolPad)))
 
+iterator progress*[T](p: var Progress, items: openArray[T]): T =
+  var spinner = newSpinny("")
+  for i in progress(p, spinner, items):
+    yield i
 
 iterator progress*[T](spinner: var Spinny, items: openArray[T]): T =
-  var pb = newProgressBar(items.len)
-  for i in progress(pb, spinner, items):
+  var p = newProgress(items.len)
+  for i in progress(p, spinner, items):
     yield i
 
 iterator progress*[T](items: openArray[T]): T =
@@ -68,12 +114,11 @@ iterator progress*[T](items: openArray[T]): T =
     yield i
 
 when isMainModule:
-  import std/[os, sequtils]
-  for i in progress((1..100).toSeq()):
-    sleep 200
+  import std/[os]
+  # for i in progress((1..100).toSeq()):
+  #   sleep 200
 
-  # var pb = newProgressBar()
-  # for i in pb.progress((1..100).toSeq()):
-    # echo pb, i
-    # sleep 100
+  var pb = newProgress(segments = @[Bar, Fraction])
+  for i in pb.progress((1..100).toSeq()):
+    sleep 100
 
