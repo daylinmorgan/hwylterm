5bf3f1cb7db28a479544c96b9596a0f3f570f9f4
Daylin Morgan <me@dayl.in>
2025-10-16 14:06:58 -0400

    add fixture based cli testing

--- tests/cli/lib.nim
+++ tests/cli/lib.nim
@@ -3,12 +3,13 @@ import std/[os, osproc, strutils, times, unittest, terminal]
 const pathToSrc = currentSourcePath().parentDir()
 const binDir = pathToSrc / "bin"
 const hwylCliSrc = pathToSrc / "../../src/hwylterm/hwylcli.nim"
+
 let hwylCliWriteTime =  getFileInfo(hwylCliSrc).lastWriteTime
 
 if not dirExists(binDir):
   createDir(binDir)
 
-proc runTestCli(module: string, args: string, code: int = 0): (string, int) =
+proc runTestCli*(module: string, args: string, code: int = 0): (string, int) =
   let cmd = binDir / module & " " & args
   let (output, code) = execCmdEx(cmd)
   result = (output.strip(), code)
@@ -19,11 +20,11 @@ proc status(s: string) =
   stdout.write(s.alignLeft(terminalWidth()).substr(0, terminalWidth()-1))
   flushFile stdout
 
-proc preCompileWorkingModule(module: string) =
+proc preCompileWorkingModule*(module: string) =
   let exe = binDir / module
   let srcModule = pathToSrc / "clis" / (module & ".nim")
   if not exe.fileExists or getFileInfo(exe).lastWriteTime < max(getFileInfo(srcModule).lastWriteTime, hwylCliWriteTime) or defined(forceSetup):
-    let cmd = "nim c -o:$1 $2" % [exe, srcModule]
+    let cmd = "nim c -d:bbAnsiOn -o:$1 $2" % [exe, srcModule]
     let (output, code) = execCmdEx(cmd)
     if code != 0:
       echo "cmd: ", cmd
@@ -41,18 +42,40 @@ proc preCompileTestModules*() =
 
   eraseLine stdout
 
-template okWithArgs*(module: string, args = "", output = "") =
-  preCompileWorkingModule(module)
-  let normalizedOutput = output.strip().strip(leading = false, chars = {'\n'}).dedent()
-  test (module & "|" & args):
-    let (actualOutput, code) = runTestCli(module, args)
-    check code == 0
+type
+  Fixture* = object
+    name*: string
+    module*: string
+    args*: string
+    ok*: bool
+    output*: string
+
+proc loadFixture*(path: string): Fixture =
+  let name = path.splitPath.tail.replace(".args", "")
+  let parts = name.split("-")
+  assert parts.len == 3
+  result.name = name
+  result.module = parts[0]
+  result.ok = parts[2] == "ok"
+  result.args = readFile(path).strip()
+
+proc loadFixtureWithOutput(path: string): Fixture =
+  result = loadFixture(path)
+  result.output = readFile(path.replace(".args",".output")).strip()
+
+
+iterator fixtures*(fixturePath: string): Fixture =
+  for (kind, path) in walkDir(fixturePath):
+    if kind != pcFile: continue
+    if path.endsWith(".args"):
+      yield loadFixtureWithOutput(path)
+
+template test*(f: Fixture) =
+  preCompileWorkingModule(f.module)
+  let normalizedOutput = f.output.strip().strip(leading = false, chars = {'\n'}).dedent()
+  test (f.module & "|" & f.args):
+    let (actualOutput, code) = runTestCli(f.module, f.args)
+    check code == (if f.ok: 0 else: 1)
     check normalizedOutput == actualOutput
 
-template failWithArgs*(module: string, args = "", output = "") =
-  preCompileWorkingModule(module)
-  let normalizedOutput = output.strip().strip(leading = false, chars = {'\n'}).dedent()
-  test (module & "|" & args):
-    let (actualOutput, code) = runTestCli(module, args)
-    check code == 1
-    check normalizedOutput == actualOutput
+
---
+++ tests/cli/touch_fixtures.nim
@@ -0,0 +1,61 @@
+import std/[os, strutils]
+import ./lib
+
+const pathToSrc = currentSourcePath().parentDir()
+const fixturePath = pathToSrc / "fixtures"
+const binDir = pathToSrc / "bin"
+
+proc touchFixture(path: string) =
+  let f = loadFixture(path)
+  # let module = parts[0]
+  # let args = readFile(path).strip()
+  preCompileWorkingModule(f.module)
+  echo f.module & " | " & f.args
+  let (output, code) = runTestCli(f.module, f.args)
+  if f.ok:
+    if code != 0: quit "expected zero exit status"
+  else:
+    if code == 0: quit "expected non-zero exit status"
+  writeFile(path.replace(".args", ".output"), output)
+
+proc touchSuite(suitePath: string) =
+  echo "updating suite: ", suitePath.splitPath.tail
+  for (kind, path) in walkDir(suitePath, checkDir = true):
+    if path.endsWith(".args"):
+      touchFixture(path)
+
+when isMainModule:
+  import hwylterm/parseopt3
+
+  proc writeHelp() =
+    echo """
+
+touch_fixtures
+  --help          show this help
+  --suite [name]
+  --fixture [fixture]
+"""
+    quit(0)
+
+  var suites: seq[string]
+  var fixtures: seq[string]
+
+  for kind, key, val in getopt():
+    case kind
+    of cmdArgument, cmdEnd, cmdError: assert(false)
+    of cmdLongOption, cmdShortOption:
+      case key
+      of "help", "h": writeHelp()
+      of "suite": suites.add val
+      of "fixture": fixtures.add val
+
+  if (suites.len + fixtures.len) == 0:
+    for (kind, path) in walkDir(fixturePath):
+      if kind == pcDir:
+        touchSuite(path)
+
+  for suite in suites:
+    touchSuite(pathToSrc / suite)
+
+  for fixture in fixtures:
+    touchFixture(pathToSrc / fixture)
