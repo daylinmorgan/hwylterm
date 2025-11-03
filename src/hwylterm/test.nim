17e820c97ea1679930ef04186f112f56b9eefb1a
Daylin Morgan <me@dayl.in>
2025-11-03 13:04:23 -0500

    remove loose file

--- src/hwylterm/test.nim
+++
@@ -1,9 +0,0 @@
-import std/macros
-macro myMacro(args: varargs[untyped]): untyped =
-  result = newStmtList()
-  for arg in args:
-    result.add quote do:
-      echo `arg`
-
-myMacro(1, 2, 3, "hello")
-
