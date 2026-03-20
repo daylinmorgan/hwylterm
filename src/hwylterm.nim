835af6557e6c934227c9369e45c54da2ca78e474
Daylin Morgan <me@dayl.in>
2026-03-20 10:48:34 -0400

    improve the landing page for the docs

--- src/hwylterm.nim
+++ src/hwylterm.nim
@@ -1,17 +1,23 @@
 ##[
+  .. importdoc:: ./hwylterm/spin/spinners
+  .. importdoc:: ./hwylterm/progress
+
   # Hwylterm
 
   see also these utility modules:
 
-  - [hwylcli](./hwylterm/hwylcli.html)
-  - [cligen adapter](./hwylterm/cligen.html), requires [cligen](https://github.com/c-blake/cligen)
-  - [chooser](./hwylterm/chooser.html)
-  - [logging](./hwylterm/logging.html)
-  - [parseopt3](./hwylterm/parseopt3.html) borrowed from `cligen`
-
+  | module | description |
+  |---|---|
+  | [hwylcli](./hwylterm/hwylcli.html) | macro to generate CLI's styled by bbansi and with parsing parseopt3 |
+  | [chooser](./hwylterm/chooser.html) | simple scrolling multi-item selecter |
+  | [logging](./hwylterm/logging.html) | wrapper for std/logging with styling |
 
   .. include:: ./docs/bbansi.md
+  .. include:: ./docs/confirm.md
+  .. include:: ./docs/spin.md
+  .. include:: ./docs/progress.md
 ]##
+
 
 import hwylterm/[spin, bbansi, confirm, progress]
 export spin, bbansi, confirm, progress
